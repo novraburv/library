2228c01e39b85bee5ac79a1d4e8c3686e5245e2e
novraburv <78609440+novraburv@users.noreply.github.com>
2022-01-09 17:33:31 +0700

    add a form to input books and their properties

--- scripts.js
+++ scripts.js
@@ -27,16 +27,55 @@ function updateTable() {
 	myLibrary.forEach(book => {
 		const tr = document.createElement('tr');
 
-		// assign index to each row
-		tr.setAttribute('data-index', `${index}`);
-		index++;
-
 		// fill the row with each book's data
 		Object.keys(book).forEach(data => {
 			const td = document.createElement('td');
 			td.textContent = book[data];
 			tr.appendChild(td);
 		})
+
+		// add delete button at the end
+		const delButton = document.createElement('button');
+		delButton.classList.add('btn', 'btn-del');
+		delButton.setAttribute('data-index', index);
+		delButton.innerHTML = '<i class="fa fa-trash"></i>';
+		index++;
+		tr.appendChild(delButton);
+
 		tbody.appendChild(tr);
 	})
 }
+
+function showForm() {
+	form.removeAttribute('hidden');
+}
+
+function hideForm() {
+	form.setAttribute('hidden', 'true')
+}
+
+function submit() {
+	addBookToLibrary(formTitle.value, formAuthor.value, formPages.value,formRead.checked);
+	resetForm();
+	hideForm();
+}
+
+function resetForm() {
+	formTitle.value = '';
+	formAuthor.value = '';
+	formPages.value = '';
+	formRead.checked = false;
+}
+
+const btnAdd = document.querySelector('.btn-add');
+const btnSubmit = document.querySelector('.btn-submit');
+const form = document.querySelector('.form');
+
+const formTitle = form.querySelector('#title');
+const formAuthor = form.querySelector('#author');
+const formPages = form.querySelector('#pages');
+const formRead = form.querySelector('#read');
+
+btnAdd.addEventListener('click', showForm);
+
+btnSubmit.addEventListener('click', submit);
