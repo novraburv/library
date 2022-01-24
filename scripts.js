2bd36bd7bc22e255aa7b99ae738ccb2a49575e85
novraburv <78609440+novraburv@users.noreply.github.com>
2022-01-24 18:55:27 +0700

    change book constructor function to be a class

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-}
+// function Book(title, author, pages, read) {
+// 	this.title = title;
+// 	this.author = author;
+// 	this.pages = pages;
+// 	this.read = read;
+// }
 
-Book.prototype = {
-	info: function () {
+// Book.prototype = {
+// 	info: function () {
+// 		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "I've read this" : "not read yet"}.`
+// 	}
+// }
+
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+	info() {
 		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "I've read this" : "not read yet"}.`
 	}
 }
